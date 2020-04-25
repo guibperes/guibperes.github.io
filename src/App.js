ec629d6081b4c789e2dde9465974189bdcb9f28b
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 16:17:03 -0300

    removed deploy test div

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import { Routes } from './routes';
 
 export function App() {
   return (
-    <>
-      <div>deploy test</div>
-      <HashRouter basename='/'>
-        <Routes />
-      </HashRouter>
-    </>
+    <HashRouter basename='/'>
+      <Routes />
+    </HashRouter>
   );
 }

257807074b7c160a56a09adba3a9f9fbb763b5e1
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 14:37:06 -0300

    rollback to browser route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Routes } from './routes';
 
 export function App() {
   return (
-    <HashRouter basename='/'>
+    <BrowserRouter>
       <Routes />
-    </HashRouter>
+    </BrowserRouter>
   );
 }

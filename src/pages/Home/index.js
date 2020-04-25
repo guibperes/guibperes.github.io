bbd8c090523de0a6bd27d6befe369db3b3d54f86
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 18:36:19 -0300

    styled home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import ReactHtmlParser from 'react-html-parser';
+import { format } from 'date-fns';
 
 import posts from '../../data.json';
-import { Page } from '../../components';
-// import { Container } from './styles';
+import {
+  Container,
+  PostContainer,
+  PostHeader,
+  PostTitle,
+  PostDate,
+  PostId,
+  PostContent,
+} from './styles';
 
 export function Home() {
   return (
-    <Page>
+    <Container>
       {posts.map((post) => (
-        <article>
-          <h3>{post.title}</h3>
-          <p>Created At {post.createdAt}</p>
-          <div>{ReactHtmlParser(post.content)}</div>
-          <br />
-        </article>
+        <PostContainer>
+          <PostHeader>
+            <PostTitle
+              to={`/${post.id}/${post.title.toLowerCase().replace(/ /g, '-')}`}
+            >
+              {post.title}
+              <PostDate>
+                {format(new Date(post.createdAt), 'dd/MM/yyyy')}
+              </PostDate>
+            </PostTitle>
+            <PostId>#{post.id}</PostId>
+          </PostHeader>
+          <PostContent>{post.description}</PostContent>
+        </PostContainer>
       ))}
-    </Page>
+    </Container>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { Page } from '../../components';
+
+export const Container = styled(Page)`
+  flex-direction: column;
+`;
+
+export const PostContainer = styled.article`
+  padding: 20px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
+
+export const PostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const PostContent = styled.p`
+  font-family: 'Comfortaa', cursive;
+  font-size: 14px;
+  padding: 10px;
+  padding-top: 20px;
+`;
+
+export const PostTitle = styled(Link)`
+  font-family: 'Comfortaa', cursive;
+  text-decoration: none;
+  color: #000;
+  font-weight: 600;
+`;
+
+export const PostDate = styled.small`
+  font-weight: 300;
+  padding-left: 5px;
+`;
+
+export const PostId = styled.small`
+  font-weight: 300;
+`;

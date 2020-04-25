c752e4df3469d27613aa7c44cc9ef02f04e843b7
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 16:09:45 -0300

    added callback to deploy

--- deploy.js
+++ deploy.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-console */
 const ghpages = require('gh-pages');
 
 const { GH_TOKEN } = process.env;
 
-ghpages.publish('build', {
-  repo: `https://${GH_TOKEN}@github.com/guibperes/my-blog`,
-});
+ghpages.publish(
+  'build',
+  {
+    repo: `https://${GH_TOKEN}@github.com/guibperes/my-blog`,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    }
+  },
+);

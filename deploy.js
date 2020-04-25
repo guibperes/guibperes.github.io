1c92adae80a1dd4c288f4613081f9d8717cd61d9
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 16:14:33 -0300

    added user to deploy

--- deploy.js
+++ deploy.js
@@ -7,6 +7,10 @@ ghpages.publish(
   'build',
   {
     repo: `https://${GH_TOKEN}@github.com/guibperes/my-blog`,
+    user: {
+      name: 'Guilherme Beidaki Peres',
+      email: 'guibperes@gmail.com',
+    },
   },
   (err) => {
     if (err) {

ac01fc56ca36220bcde6439f59ce829693a59e11
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-04-25 14:29:36 -0300

    added deploy to gh pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { Routes } from './routes';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
